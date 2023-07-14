09be486d1870075c6906402af97b5f845d805a9e
BOOMHUNK <51675127+BOOMHUNK@users.noreply.github.com>
2023-07-14 14:43:01 +0430

    fixed alias for react/jsx-dev-runtime.js

--- webpack.config.js
+++ webpack.config.js
@@ -153,7 +153,7 @@ module.exports = {
   resolve: {
     alias: {
       // components: path.resolve(__dirname, 'src/components'),
-      'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+      'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
       'react/jsx-runtime.js': 'react/jsx-runtime',
     },
     modules: ['node_modules'],
