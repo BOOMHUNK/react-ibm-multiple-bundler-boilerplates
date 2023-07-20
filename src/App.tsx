d88dc185320117ea2f2e7651d9dafb534a1c27ea
BOOMHUNK <51675127+BOOMHUNK@users.noreply.github.com>
2023-07-20 13:53:12 +0430

    - feat: added prefix support for FluidTextInput - fix: made the FluidTextInput dynamically resize to keep the suffix and prefix inside

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { Content, SelectItem } from '@carbon/react';
 import { FluidSelect, FluidTextInput } from './components/atoms';
 
 function generateSuffix(unitName: string) {
-  return unitName == 'kg' ? 'Gram' : unitName == 'lbs' ? 'lbs' : '';
+  return unitName == 'kg'
+    ? 'Gram'
+    : unitName == 'lbs'
+    ? 'lbs'
+    : unitName == 'other'
+    ? 'suffix with any length'
+    : '';
 }
 
 const App = (): JSX.Element => {
@@ -15,9 +21,11 @@ const App = (): JSX.Element => {
           id={`select-2`}
           labelText="Select an option"
           onChange={(e) => setAmountSuffix(generateSuffix(e.target.value))}
+
         >
           <SelectItem value="kg" text="kg" />
           <SelectItem value="lbs" text="lbs" />
+          <SelectItem value="other" text="suffix with any length" />
         </FluidSelect>
 
         <FluidTextInput
@@ -25,6 +33,16 @@ const App = (): JSX.Element => {
           labelText="Amount"
           id="text-input-2"
           suffix={amountSuffix}
+          // warn={true}
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <FluidTextInput
+          type="text"
+          labelText="Amount"
+          id="text-input-3"
+          suffix="suffix with any length"
+          prefix="prefix"
         />
       </div>
     </Content>
--- src/components/atoms/FluidTextInput/FluidTextInput.tsx
+++ src/components/atoms/FluidTextInput/FluidTextInput.tsx
@@ -1,16 +1,62 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import { FluidForm, TextInput } from '@carbon/react';
 import { TextInputProps } from 'carbon-components-react';
+import useResizeObserver from '../../../hooks/useResizeObserver';
 
 interface Props extends TextInputProps {
   suffix?: string;
 }
 
-export default function FluidTextInput(props: Props): ReactElement {
+export default function FluidTextInput({
+  suffix,
+  prefix,
+  ...props
+}: Props): ReactElement {
+  const [suffixRef, suffixSize] = useResizeObserver<HTMLElement>();
+  const [prefixRef, prefixSize] = useResizeObserver<HTMLElement>();
+
   return (
-    <FluidForm style={{position: "relative"}}>
-      <TextInput {...props}  style={{padding:"32px 54px 13px 16px",}}/>
-      {props.suffix && <span style={{position: "absolute", right: "16px", bottom: "16px",color: "#525252", fontSize:"12px", userSelect:"none" }}>{props.suffix}</span>}
+    <FluidForm style={{ position: 'relative' }}>
+      <TextInput
+        {...props}
+        style={{
+          padding: `32px ${
+             suffix && suffixSize.width ? suffixSize.width + 24 : '16'
+          }px 13px ${
+            prefix && prefixSize.width ? prefixSize.width + 24  : '16'
+          }px`,
+        }}
+      />
+      {prefix && (
+        <span
+          ref={prefixRef}
+          style={{
+            position: 'absolute',
+            left: '16px',
+            bottom: '16px',
+            color: '#525252',
+            fontSize: '12px',
+            userSelect: 'none',
+          }}
+        >
+          {prefix}
+        </span>
+      )}
+      {suffix && (
+        <span
+          ref={suffixRef}
+          style={{
+            position: 'absolute',
+            right: '16px',
+            bottom: '16px',
+            color: '#525252',
+            fontSize: '12px',
+            userSelect: 'none',
+          }}
+        >
+          {suffix}
+        </span>
+      )}
     </FluidForm>
   );
 }
