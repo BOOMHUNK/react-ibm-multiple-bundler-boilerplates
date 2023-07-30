a5c5e6895e80f29539cec5af386b513623ffd58f
BOOMHUNK <BOOMHUNK@Gmail.com>
2023-07-30 08:31:39 +0330

    - Overcame the main challenges easily

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Content,
   SelectItem,
@@ -8,21 +8,37 @@ import {
   Column,
   Button,
   Row,
-} from '@carbon/react';
-import { FluidSelect, FluidTextInput } from './components/atoms';
-import './app.scss';
+  Stack,
+} from "@carbon/react";
+
+import { ArrowRight } from "@carbon/react/icons";
+
+import "./app.scss";
 
 const App = (): JSX.Element => {
   return (
     <Content>
-      <Grid fullWidth>
-        <Column xlg={16} lg={16} md={8} sm={4}>
-          
-          <Heading className="BigHeader InvertedTextColor">
-            You Can Have Non Of These Problems With Fabizi!
-          </Heading>
-          <div id='DarkRightPushedDiv'><span>In publishing and graphic design, Lorem ipsum is aIn publishing and graphic design, Lorem ipsum is In publishing and graphic design, Lorem ipsum is aIn publishing and</span><Button><span>Try it now</span></Button></div>
+      <Grid style={{ position: "relative" }}>
+        {/* <Row> */}
+        <Column xlg={2} lg={3} md={2} sm={1} />
+        <Column xlg={14} lg={13} md={6} sm={3}>
+          <Stack className="darkBG">
+            <Heading className="BigHeader InvertedTextColor">
+              You Can Have Non Of These Problems With Fabizi!
+            </Heading>
+            <div id="ContentDiv">
+              <span className="">
+                In publishing and graphic design, Lorem ipsum is aIn publishing
+                and graphic design, Lorem ipsum is In publishing and graphic
+                design, Lorem ipsum is aIn publishing and
+              </span>
+              <Button renderIcon={ArrowRight}>
+                <span>Try it now</span>
+              </Button>
+            </div>
+          </Stack>
         </Column>
+        {/* </Row> */}
       </Grid>
     </Content>
   );
