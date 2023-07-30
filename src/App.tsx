6274ef1a1b8037b35785ff7ffbfecdba59a97635
BOOMHUNK <51675127+BOOMHUNK@users.noreply.github.com>
2023-07-30 23:49:45 +0430

    - feat: FinalizedDarkInvertedSection Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,21 @@
-import { Content, Heading, Grid, Column, Button, Stack } from "@carbon/react";
+import { Content, Heading, Grid, Column, Button, Stack } from '@carbon/react';
 
-import { ArrowRight } from "@carbon/react/icons";
+import { ArrowRight } from '@carbon/react/icons';
 
-import "./app.scss";
+import './app.scss';
+import { DarkInvertedSection } from './components/molecules';
 
 const App = (): JSX.Element => {
   return (
     <Content>
-      <Grid className="testimonial-problemsGone">
-        {/* <Row> */}
-        <Column xlg={2} lg={3} md={1} sm={0} />
-        <Column xlg={14} lg={13} md={7} sm={4}>
-          <Stack className="darkBg">
-            <Heading className="invertedText">
-              You Can Have Non Of These Problems With Fabizi!
-            </Heading>
-            <div className="contentDiv">
-              <Grid fullWidth>
-                <Column xlg={10} lg={10} md={7} sm={4}>
-                  <span className="">
-                    In publishing and graphic design, Lorem ipsum is a In
+      <DarkInvertedSection
+        Title="You Can Have Non Of These Problems With Fabizi!"
+        Desc="In publishing and graphic design, Lorem ipsum is a In
                     publishing and graphic design, Lorem ipsum is In publishing
-                    and graphic design, Lorem ipsum is aIn publishing and
-                  </span>
-                </Column>
-                <Column lg={0} md={4} />
-                <Column xlg={4} lg={3} md={3} sm={4}>
-                  <Button className="button" renderIcon={ArrowRight}>
-                    Try it now
-                  </Button>
-                </Column>
-              </Grid>
-            </div>
-          </Stack>
-        </Column>
-        {/* </Row> */}
-      </Grid>
+                    and graphic design, Lorem ipsum is aIn publishing and"
+        ButtonTitle="Try it now"
+        ButtonAction={(e) => console.log('Clicked!')}
+      />
     </Content>
   );
 };
---
+++ src/components/molecules/DarkInvertedSection/DarkInvertedSection.tsx
@@ -0,0 +1,48 @@
+import { ReactElement } from 'react';
+import { Heading, Grid, Column, Button, Stack } from '@carbon/react';
+
+import { ArrowRight } from '@carbon/react/icons';
+import './_darkInvertedSection.scss';
+
+interface Props {
+  Title: string;
+  Desc: string;
+  ButtonTitle?: string;
+  ButtonAction?: React.MouseEventHandler<HTMLButtonElement> | undefined;
+}
+
+export default function DarkInvertedSection({
+  Title,
+  Desc,
+  ButtonTitle,
+  ButtonAction,
+}: Props): ReactElement {
+  return (
+    <Grid fullWidth className="testimonial-problemsGone">
+      <Column xlg={2} lg={3} md={1} sm={0} />
+      <Column xlg={14} lg={13} md={7} sm={4}>
+        <Stack className="darkBg">
+          <Heading className="invertedText">{Title}</Heading>
+          <div className="content-grid-container">
+            <Grid>
+              <Column lg={1} />
+              <Column xlg={9} lg={9} md={7} sm={4} className="desc-col">
+                <span>{Desc}</span>
+              </Column>
+              <Column lg={0} md={4} />
+              <Column xlg={4} lg={3} md={3} sm={4} className="btn-col">
+                <Button
+                  className="button"
+                  renderIcon={ArrowRight}
+                  onClick={(e) => ButtonAction && ButtonAction(e)}
+                >
+                  {ButtonTitle}
+                </Button>
+              </Column>
+            </Grid>
+          </div>
+        </Stack>
+      </Column>
+    </Grid>
+  );
+}
