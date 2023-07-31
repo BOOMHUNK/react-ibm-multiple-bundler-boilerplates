98d9ff4857db06cb25bc75c40238d023e517c68b
BOOMHUNK <BOOMHUNK@Gmail.com>
2023-07-31 06:36:21 +0330

    - chore: Cleanup

--- src/components/molecules/DarkInvertedSection/DarkInvertedSection.tsx
+++ src/components/molecules/DarkInvertedSection/DarkInvertedSection.tsx
@@ -19,18 +19,18 @@ export default function DarkInvertedSection({
 }: Props): ReactElement {
   return (
     <Grid fullWidth className="testimonial-problemsGone">
-      <Column xlg={2} lg={3} md={1} sm={0} />
-      <Column xlg={14} lg={13} md={7} sm={4}>
+      <Column xlg={1} lg={1} md={1} sm={0} />
+      <Column xlg={15} lg={15} md={7} sm={4}>
         <Stack className="darkBg">
           <Heading className="invertedText">{Title}</Heading>
           <div className="content-grid-container">
             <Grid>
               <Column lg={1} />
-              <Column xlg={9} lg={9} md={7} sm={4} className="desc-col">
+              <Column lg={9} md={7} sm={4} className="desc-col">
                 <span>{Desc}</span>
               </Column>
               <Column lg={0} md={4} />
-              <Column xlg={4} lg={3} md={3} sm={4} className="btn-col">
+              <Column lg={5} md={3} sm={4} className="btn-col">
                 <Button
                   className="button"
                   renderIcon={ArrowRight}
