01fcbe8de65788ae91f5acda72b73b7b545de252
BOOMHUNK <BOOMHUNK@Gmail.com>
2023-08-01 12:09:42 +0330

    - performance: Improved useWindowResize hook

--- src/hooks/useWindowResize.ts
+++ src/hooks/useWindowResize.ts
@@ -1,25 +1,28 @@
 import { useState, useEffect } from 'react';
+import useDebouncedCallback from './useDebounce';
 
-function useWindowResize() {
+function useWindowResize(delay = 0) {
     const [windowSize, setWindowSize] = useState<{ width: number | undefined, height: number | undefined }>({
         width: undefined,
         height: undefined
     });
 
+    function handleResize() {
+        setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight
+        })
+    }
+    const debouncedHandleResize = useDebouncedCallback(handleResize, delay);
+
     useEffect(() => {
-        function handleResize() {
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight
-            });
-        }
+        window.addEventListener('resize', debouncedHandleResize);
 
-        window.addEventListener('resize', handleResize);
+        // Initial resize
+        debouncedHandleResize();
 
-        handleResize();
-
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+        return () => window.removeEventListener('resize', debouncedHandleResize);
+    }, [debouncedHandleResize]);
 
     return windowSize;
 }
